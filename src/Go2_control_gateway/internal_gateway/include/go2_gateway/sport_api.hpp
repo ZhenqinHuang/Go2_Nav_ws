aa7e02914121f9d0e597482f3cfd1ed83b84f979
ZhenqinHuang <zhenqinhuang758@gmail.com>
2026-07-28 19:31:59 +0800

    feat: add Unitree SDK UDP gateway daemon

--- src/Go2_control_gateway/internal_gateway/include/go2_gateway/sport_api.hpp
+++ src/Go2_control_gateway/internal_gateway/include/go2_gateway/sport_api.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <memory>
+#include <string>
+
 namespace go2_gateway {
 
 class SportApi {
@@ -9,5 +12,9 @@ class SportApi {
   virtual int Move(float vx, float vy, float vyaw) = 0;
   virtual int StopMove() = 0;
 };
+
+std::unique_ptr<SportApi> make_dry_run_sport_api();
+std::unique_ptr<SportApi> make_unitree_sport_api(
+    const std::string& interface_name);
 
 }  // namespace go2_gateway
---
+++ src/Go2_control_gateway/internal_gateway/src/unitree_sport_api.cpp
@@ -0,0 +1,36 @@
+#include "go2_gateway/sport_api.hpp"
+
+#include <unitree/robot/channel/channel_factory.hpp>
+#include <unitree/robot/go2/sport/sport_client.hpp>
+
+#include <memory>
+#include <string>
+
+namespace go2_gateway {
+
+class UnitreeSportApi final : public SportApi {
+ public:
+  explicit UnitreeSportApi(const std::string& interface_name) {
+    unitree::robot::ChannelFactory::Instance()->Init(0, interface_name);
+    client_.SetTimeout(3.0F);
+    client_.Init();
+  }
+
+  int BalanceStand() override { return client_.BalanceStand(); }
+
+  int Move(float vx, float vy, float vyaw) override {
+    return client_.Move(vx, vy, vyaw);
+  }
+
+  int StopMove() override { return client_.StopMove(); }
+
+ private:
+  unitree::robot::go2::SportClient client_;
+};
+
+std::unique_ptr<SportApi> make_unitree_sport_api(
+    const std::string& interface_name) {
+  return std::make_unique<UnitreeSportApi>(interface_name);
+}
+
+}  // namespace go2_gateway
